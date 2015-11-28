2ecf2f57169982522b8d0fde18b679d77eccc435
ayagamal <ayatu-allah.gamal@aybasu.com>
2015-11-28 19:18:16 +0200

    server parser

---
+++ server/src/server/XmlParser.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+ 
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
+ 
+public class XmlParser
+{
+    public ArrayList parseXml(InputStream in)
+    {
+        //Create a empty link of users initially
+        ArrayList<User> users = new ArrayList<User>();
+        ArrayList<Group> groups = new ArrayList<Group>();
+        try
+        {
+            //Create default handler instance
+            ParserHandler handler = new ParserHandler();
+ 
+            //Create parser from factory
+            XMLReader parser = XMLReaderFactory.createXMLReader();
+ 
+            //Register handler with parser
+            parser.setContentHandler(handler);
+ 
+            //Create an input source from the XML input stream
+            InputSource source = new InputSource(in);
+ 
+            //parse the document
+            parser.parse(source);
+ 
+            //populate the parsed users list in above created empty list; You can return from here also.
+            users = handler.getUsers();
+            groups = handler.getGroups();
+ 
+        } catch (SAXException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+ 
+        }
+        return users;
+        
+    }
+     public ArrayList parseGroupsXml(InputStream in)
+    {
+        //Create a empty link of users initially
+
+        ArrayList<Group> groups = new ArrayList<Group>();
+        try
+        {
+            //Create default handler instance
+            ParserHandler handler = new ParserHandler();
+ 
+            //Create parser from factory
+            XMLReader parser = XMLReaderFactory.createXMLReader();
+ 
+            //Register handler with parser
+            parser.setContentHandler(handler);
+ 
+            //Create an input source from the XML input stream
+            InputSource source = new InputSource(in);
+ 
+            //parse the document
+            parser.parse(source);
+ 
+            //populate the parsed users list in above created empty list; You can return from here also.
+      
+            groups = handler.getGroups();
+ 
+        } catch (SAXException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+ 
+        }
+        return groups;
+        
+    }
+}
+
--- server/src/server/server.java
+++ server/src/server/server.java
@@ -5,15 +5,83 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+
 
 
 public class server {
-
+   /* public server() throws FileNotFoundException
+    {
+    	 
+    	        //Locate the file
+    	     
+    	        File xmlFile = new File("users.txt");
+    	        File xmlGroupsFile = new File("groups.txt");
+    	        //System.out.print(xmlFile);
+    	        
+    	        //Create the parser instance
+    	        XmlParser parser = new XmlParser();
+    	 
+    	        //Parse the file
+    	       ArrayList users = parser.parseXml(new FileInputStream(xmlFile));
+    	       ArrayList groups = parser.parseGroupsXml(new FileInputStream(xmlGroupsFile));
+    	       
+    	      for(int i=0; i<users.size(); i++)
+    	       {  
+    	           
+    	          System.out.print((users.toArray())[i].toString());
+    	       }
+    	      for(int i=0; i<groups.size(); i++)
+    	       {  
+    	           
+    	          System.out.print((groups.toArray())[i].toString());
+    	       }
+    	              
+    	 
+    	        //Verify the result
+    	        
+    	    
+    }*/
 	
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+    	
+    	
+    	 File xmlFile = new File("users.txt");
+	        File xmlGroupsFile = new File("groups.txt");
+	        //System.out.print(xmlFile);
+	        
+	        //Create the parser instance
+	        XmlParser parser = new XmlParser();
+	 
+	        //Parse the file
+	       ArrayList users = parser.parseXml(new FileInputStream(xmlFile));
+	       ArrayList groups = parser.parseGroupsXml(new FileInputStream(xmlGroupsFile));
+	       
+	      for(int i=0; i<users.size(); i++)
+	       {  
+	           
+	          System.out.print((users.toArray())[i].toString());
+	       }
+	      for(int i=0; i<groups.size(); i++)
+	       {  
+	           
+	          System.out.print((groups.toArray())[i].toString());
+	       }
+	              
+	 
+	        //Verify the result
+	        
+	    
         // TODO code application logic here
-        try {
+      //  try {
         /*	//initiate list of groups
         	ArrayList<group> groups= new ArrayList<group>();
         	groups.add(new group("Test Group 1"));
@@ -30,15 +98,15 @@ public class server {
         	admins.add(new admin("hamda","adminPASS",false,"192.168.1.5"));
        */ 	
         	//list of logged in clients
-        	ArrayList<Socket> activeClients=new ArrayList<Socket>();
+        	//ArrayList<Socket> activeClients=new ArrayList<Socket>();
         	
         	
         	
             //1.Create Server Socket
-            ServerSocket sv = new ServerSocket(1243);
+            //ServerSocket sv = new ServerSocket(1243);
             
             //Server is always On
-            while (true) {
+           /* while (true) {
             //2.Listen for Clients
                 Socket c;
                 c = sv.accept();
@@ -52,6 +120,7 @@ public class server {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-    }
+    }*/
 
+}
 }
