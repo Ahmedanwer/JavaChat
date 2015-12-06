6f0046cf1dce4173c7a599ad2877a19cf0ff26fc
ahmedanwer <ahmedmuhanwar@gmail.com>
2015-12-06 20:11:54 +0200

    changing ip algorithm

--- client/src/client/apiFunctions.java
+++ client/src/client/apiFunctions.java
@@ -68,12 +68,7 @@ public class apiFunctions {
 	   	  obj.put("header", "login");
 	      obj.put("username", user);
 	      obj.put("password", pass);
-	      try {
-			obj.put("ipAdress", InetAddress.getLocalHost().getHostAddress().toString());
-		} catch (UnknownHostException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
+	     
 	      
 	      try {
 				Sdos.writeUTF(obj.toJSONString());
